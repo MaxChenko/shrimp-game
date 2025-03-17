e526a77949dcf15e93aa162c2eb89d278c193f67
Madison Fehr <madison.fehr@nhd.com>
2025-03-17 16:11:21 -0500

    started shrimp class / types

---
+++ Assets/Scripts/Data/Shrimp/ShrimpTypes.cs
@@ -0,0 +1,60 @@
+﻿public enum Grade
+{
+    SSS,
+    SS,
+    S,
+    A,
+    B,
+    C
+}
+
+public enum Maturity
+{
+    Larva,
+    Juvenile,
+    Adult
+}
+
+public enum ActivityLevel
+{
+    Low,
+    Medium,
+    High
+}
+
+public enum Rarity
+{
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary
+}
+
+public enum Size
+{
+    Tiny,
+    Small,
+    Medium,
+    Large
+}
+
+public interface ISpecies
+{
+    decimal BaseValue { get; }
+    Rarity Rarity { get; }
+    Size Size { get; }
+    float Transparency { get; }
+    bool Bioluminescence { get; }
+    float Fertility { get; }
+    int GestationTime { get; }
+    int EggCount { get; }
+    ActivityLevel ActivityLevel { get; }
+    float MinTemperatureTolerance { get; }
+    float MaxTemperatureTolerance { get; }
+    float MinPHTolerance { get; }
+    float MaxPHTolerance { get; }
+    
+    // WaterType WaterType { get; }
+    // Food[] Diet { get; }
+}
